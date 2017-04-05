ad2c3fe9ff6fba0bd22225e1f56105bd9c915f45
Stefan Pesic <sp.pesic@live.nl>
2017-04-05 21:00:51 +0200

    Buttons, text, and picture positions
    Buttons, text, and picture are on the right positions

---
+++ actual_testing.py
@@ -0,0 +1,11 @@
+#Here the actual testing will takes place
+from tkinter import *
+
+#Background gets loaded
+window = TK()
+canvas = Canvas(window,width=500,height=500)
+canvas.grid()
+
+
+
+root.mainloop()
--- testing.py
+++ testing.py
@@ -1,28 +1,23 @@
+#Needed
 from tkinter import *
-from tkinter import ttk
-
-
 
 #Background gets loaded
-root = Tk()
-root.title("Rotterdam Living")
+root = Tk() #Needed to run
+root.title("Rotterdam Living") #Window title
+root.geometry('{}x{}'.format(root.winfo_screenwidth(), root.winfo_screenheight())) #allows options boxes(minimize etc)
 
-
-
-
-
-#gets current screen size
+#gets user's screen size
 screenx=root.winfo_screenwidth()
 screeny= root.winfo_screenheight()
 
 #The image
-image = PhotoImage(file="Rotterdamse_kaart.png")
-larger_image = image.zoom(1)
-label = Label(image=larger_image,background="red")
-
-label.grid()
-
-
+image = PhotoImage(file="Rotterdamse_kaart.png") #The file
+larger_image = image.zoom(2) #resizes image but there must be a better way //TODO find better way to resize
+label = Label(root, image=larger_image) #saves the image
+label.columnconfigure(0,weight=30000)
+label.grid(sticky=N,column=1, row=1,rowspan=999) #loads the image in row 1 of column 1. Sticky is the position
+#Sticky = position, column = in which column, row = in which row.
+#Grid is similar to Microsoft Excel, the samer type of layout
 
 #Text
 overschie = "Overschie" #the name of the area's that will get loaded when a user clicks a button
@@ -36,16 +31,19 @@ waalhaven = "Waalhaven"
 charlois = "Charlois"
 feijenood = "Feijenoord"
 ijsselmonde = "Ijsselmonde"
+rotterdam = "Rotterdam"
 
-def textChanger(): #method that is going to change the text based on the user's input
+text = Label(root,  width=0, height=1,text=charlois,font=(rotterdam,35,"bold"),) #puts image on screen
+text.grid(row=0,column=1,sticky=N) #loads the image
+
+
+#methods that are going to run when the an user clicks a certain button
+def textChanger():
     pass
-
-text = Label(root,  width=60, height=5,text=charlois,font=("arial",35,"bold"),)
-text.grid()
 
 #Methods that run when a button is pressed
 def milieuMethod():
-   pass
+    pass
 
 def luchtkwaliteitMethod():
    pass
@@ -69,34 +67,44 @@ def backMethod():
     pass
 
 
-#Buttons
-Button1 = Button(root, text ="Milieu", command = milieuMethod)
-Button1.grid(row=0, column=1, columnspan=1, sticky=W+E,ipady=10) #information you can set about a button
+#The buttons
+Button1 = Button(root, text ="Milieu", command = milieuMethod,font=("arial",30,"bold")) #command = method
+Button1.columnconfigure(0,weight=3) #weight = if it will get moved when another button takes wide rows
+Button1.grid(row=1, column=0, sticky=W,ipadx=screenx/20,ipady=screeny/75) #information you can set about a button
+#ipadx = the size in width, ipady = the size it takes in height
 
-Button2 = Button(root, text ="Luchtkwaliteit", command = luchtkwaliteitMethod())
-Button2.grid(row=1, column=1, columnspan=1,sticky=W+E,ipady=30,ipadx=30)
+Button2 = Button(root, text ="Luchtkwaliteit", command = luchtkwaliteitMethod(),font=("arial",30,"bold"))
+Button2.columnconfigure(0,weight=3)
+Button2.grid(row=2, column=0, sticky=W, ipadx=screenx/175,ipady=screeny/75)
 
-Button3 = Button(root, text ="Veiligheid", command = veiligheidMethod())
-Button3.grid(row=2, column=1, columnspan=1,sticky=W+E,ipady=30)
+Button3 = Button(root, text ="Veiligheid", command = veiligheidMethod(),font=("arial",30,"bold"))
+Button3.columnconfigure(0,weight=3)
+Button3.grid(row=3, column=0, sticky=W, ipadx=screenx/35, ipady=screeny/75)
 
-Button4 = Button(root, text ="Voorzieningen", command = voorzieningenMethod())
-Button4.grid(row=3, column=1, columnspan=1,sticky=W+E,ipady=30)
+Button4 = Button(root, text ="Voorzieningen", command = voorzieningenMethod(),font=("arial",30,"bold"))
+Button4.columnconfigure(0,weight=3)
+Button4.grid(row=4, column=0,sticky=W,ipadx=screenx/300, ipady=screeny/75)
 
-Button5 = Button(root, text ="Tevredenheid", command = tevredenheidMethod())
-Button5.grid(row=4, column=1, columnspan=1,sticky=W+E,ipady=30)
+Button5 = Button(root, text ="Tevredenheid", command = tevredenheidMethod(),font=("arial",30,"bold"))
+Button5.columnconfigure(0,weight=3)
+Button5.grid(row=5, column=0,sticky=W,ipadx=screenx/100, ipady=screeny/75)
 
-Button6 = Button(root, text ="Gemiddelde huurpprijs", command = huurprijsMethod())
-Button6.grid(row=5, column=1, columnspan=1,sticky=W+E)
+Button6 = Button(root, text ="Huurpprijs", command = huurprijsMethod(),font=("arial",30,"bold"))
+Button6.columnconfigure(0,weight=3)
+Button6.grid(row=6, column=0,sticky=W,ipadx=screenx/40, ipady=screeny/75)
 
-Button7 = Button(root, text ="Gemiddelde koopprijs", command = koopprijsMethod())
-Button7.grid(row=6, column=1, columnspan=1,sticky=W+E)
+Button7 = Button(root, text ="Koopprijs", command = koopprijsMethod(),font=("arial",30,"bold"))
+Button7.columnconfigure(0,weight=5)
+Button7.grid(row=7, column=0, sticky=W,ipadx=screenx/35, ipady=screeny/75)
 
-Button8 = Button(root, text ="Back", command = backMethod())
-Button8.grid(row=6, column=1, columnspan=1,sticky=W+E)
+Button8 = Button(root, text ="Back", command = backMethod(),font=("arial",30,"bold"))
+Button8.columnconfigure(0,weight=3)
+Button8.grid(row=8, column=0, sticky=W,ipadx=screenx/18, ipady=screeny/75)
 
-#Mouse position
+#Catches mouse position
 x = root.winfo_pointerx()
 y = root.winfo_pointery()
+
 
 #Loops
 root.mainloop()
