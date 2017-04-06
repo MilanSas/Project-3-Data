b4361845b21a78d25d1e09acc34f8a1567f5367b
Stefan Pesic <sp.pesic@live.nl>
2017-04-06 21:15:16 +0200

    Map, text
    Map is now in the screen, text is in the screen and polygon selector
    method added

--- RotterdamLiving.py
+++ RotterdamLiving.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 
 
@@ -8,6 +9,7 @@ root = Tk() #Needed to run
 #TODO methods needs to be filled in
 #TODO transparant buttons for the locations
 #TODO find better way to resize the picture
+#TODO Instead of coordinations, make tkinter detect the colour and then change the area based on it
 
 
 
@@ -59,9 +61,6 @@ Button7_bool = False
 Button8_bool = False
 
 
-#The text that will appear on top
-text = Label(root,width=0, height=1,text=rotterdam,font=("Helvetica",35,"bold"),) #puts image on screen
-text.grid(row=0,column=1,sticky=N) #loads the image
 
 
 
@@ -157,28 +156,15 @@ def backMethod():
 
 #catches current position of the mouse and changes name based on mouse position
 #This method will also seve as to detect were the user clicks on the map
-def motion(event):
-    print("Mouse position: (%s %s)" %(event.x,event.y))
-    if event.x < 500 and event.y < 279: #the mouse positions
-        text.config(text=overschie) #changes the text that appears on the top
-    elif event.x > 550 and event.y < 279:
-        text.config(text=hillegersberg_schiebroek)
-    else:
-        text.config(text=rotterdam)
 
-#commented out, belongs to the image
-# label.bind('<Motion>', motion)  # mouse method applied to the label(picture) object. Only when on the picture, the method will be activated
-
-
+#
 #===================================================================================================================
 #The polygon below
-
-
-canvas = Canvas(root, width=screenx, height=screeny)
+canvas = Canvas(root, width=screenx,height=screeny)
 
 def rs(size):
-    ratio = 1080 / size
-    return (screeny / ratio) - 50
+    ratio = (screeny + screeny/2) / size
+    return (screeny / ratio)
 
 def changecolor(object,color):
     canvas.itemconfig(object.shape, fill=HexToRGB(color))
@@ -193,23 +179,33 @@ class polygon:
         self.color = color
         self.shape = canvas.create_polygon(list, fill=HexToRGB(self.color), outline='black', width = 2)
 
-overschie = polygon((20,50,120),(rs(904),rs(194),rs(947),rs(183),rs(955),rs(205),rs(1106),rs(109),rs(1176),rs(199),rs(1222),rs(163),rs(1242),rs(185),rs(1237),rs(355),rs(1109),rs(432),rs(1138),rs(471),rs(1057),rs(479),rs(1017),rs(403),rs(1059),rs(350),rs(1050),rs(345),rs(1003),rs(366),rs(964),rs(322),rs(949),rs(342)))
-hillegersberg = polygon((20,50,120),(rs(1242),rs(185),rs(1237),rs(355),rs(1520),rs(282),rs(1499),rs(189),rs(1475),rs(213),rs(1432),rs(195),rs(1428),rs(172),rs(1387),rs(141),rs(1382),rs(154),rs(1280),rs(90)))
-prins_alexander = polygon((20,50,120),(rs(1562),rs(454),rs(1661),rs(425),rs(1651),rs(350),rs(1693),rs(337),rs(1650),rs(227),rs(1663),rs(222),rs(1673),rs(233),rs(1821),rs(141),rs(1791),rs(116),rs(1815),rs(31),rs(1804),rs(12),rs(1617),rs(76),rs(1680),rs(145),rs(1627),rs(169),rs(1595),rs(134),rs(1499),rs(189)))
-kralingen = polygon((20,50,120),(rs(1521),rs(282),rs(1562),rs(454),rs(1538),rs(498),rs(1572),rs(594),rs(1489),rs(616),rs(1469),rs(593),rs(1453),rs(522),rs(1397),rs(500),rs(1382),rs(461),rs(1337),rs(454),rs(1331),rs(330)))
-noord = polygon((20,50,120),(rs(1331),rs(330),rs(1237),rs(355),rs(1109),rs(432),rs(1136),rs(468),rs(1206),rs(462),rs(1208),rs(455),rs(1288),rs(443),rs(1337),rs(454)))
-delftshaven = polygon((20,50,120),(rs(1057),rs(479),rs(1034),rs(546),rs(1048),rs(582),rs(1038),rs(610),rs(1113),rs(596),rs(1227),rs(620),rs(1262),rs(614),rs(1244),rs(558),rs(1223),rs(553),rs(1247),rs(539),rs(1243),rs(456)))
-centrum = polygon((20,50,120),(rs(1262),rs(614),rs(1244),rs(558),rs(1223),rs(553),rs(1247),rs(539),rs(1243),rs(461),rs(1206),rs(462),rs(1208),rs(455),rs(1288),rs(443),rs(1337),rs(454),rs(1382),rs(461),rs(1397),rs(500),rs(1375),rs(506),rs(1322),rs(574)))
-feijenoord = polygon((20,50,120),(rs(1397),rs(500),rs(1375),rs(506),rs(1322),rs(574),rs(1262),rs(614),rs(1288),rs(642),rs(1378),rs(617),rs(1372),rs(694),rs(1361),rs(698),rs(1361),rs(720),rs(1417),rs(772),rs(1454),rs(735),rs(1487),rs(735),rs(1496),rs(698),rs(1450),rs(605),rs(1469),rs(593),rs(1453),rs(522)))
-ijsselmonde = polygon((20,50,120),(rs(1572),rs(594),rs(1489),rs(616),rs(1469),rs(593),rs(1450),rs(605),rs(1496),rs(698),rs(1487),rs(735),rs(1454),rs(735),rs(1417),rs(772),rs(1457),rs(824),rs(1524),rs(831),rs(1574),rs(813),rs(1657),rs(766),rs(1666),rs(779),rs(1682),rs(769),rs(1712),rs(606),rs(1625),rs(584)))
-charlois = polygon((20,50,120),(rs(1262),rs(614),rs(1288),rs(642),rs(1378),rs(617),rs(1372),rs(694),rs(1361),rs(698),rs(1361),rs(720),rs(1417),rs(772),rs(1457),rs(824),rs(1388),rs(820),rs(1315),rs(848),rs(1315),rs(869),rs(1242),rs(871),rs(1161),rs(839),rs(1201),rs(824),rs(1218),rs(800),rs(1201),rs(792),rs(1199),rs(763),rs(1246),rs(656),rs(1225),rs(646),rs(1227),rs(620)))
-waalhaven = polygon((20,50,120),(rs(1161),rs(839),rs(1201),rs(824),rs(1218),rs(800),rs(1201),rs(792),rs(1199),rs(763),rs(1246),rs(656),rs(1225),rs(646),rs(1227),rs(620),rs(1113),rs(596),rs(1038),rs(610),rs(954),rs(639),rs(862),rs(618),rs(885),rs(658),rs(917),rs(663),rs(921),rs(647),rs(972),rs(670),rs(962),rs(722),rs(937),rs(743),rs(934),rs(764),rs(923),rs(765),rs(927),rs(797),rs(1048),rs(795)))
+overschie_polgon = polygon((20,50,120),(rs(904),rs(194),rs(947),rs(183),rs(955),rs(205),rs(1106),rs(109),rs(1176),rs(199),rs(1222),rs(163),rs(1242),rs(185),rs(1237),rs(355),rs(1109),rs(432),rs(1138),rs(471),rs(1057),rs(479),rs(1017),rs(403),rs(1059),rs(350),rs(1050),rs(345),rs(1003),rs(366),rs(964),rs(322),rs(949),rs(342)))
+hillegersberg_polygon = polygon((20,50,120),(rs(1242),rs(185),rs(1237),rs(355),rs(1520),rs(282),rs(1499),rs(189),rs(1475),rs(213),rs(1432),rs(195),rs(1428),rs(172),rs(1387),rs(141),rs(1382),rs(154),rs(1280),rs(90)))
+prins_alexander_polygon = polygon((20,50,120),(rs(1562),rs(454),rs(1661),rs(425),rs(1651),rs(350),rs(1693),rs(337),rs(1650),rs(227),rs(1663),rs(222),rs(1673),rs(233),rs(1821),rs(141),rs(1791),rs(116),rs(1815),rs(31),rs(1804),rs(12),rs(1617),rs(76),rs(1680),rs(145),rs(1627),rs(169),rs(1595),rs(134),rs(1499),rs(189)))
+kralingen_polygon = polygon((20,50,120),(rs(1521),rs(282),rs(1562),rs(454),rs(1538),rs(498),rs(1572),rs(594),rs(1489),rs(616),rs(1469),rs(593),rs(1453),rs(522),rs(1397),rs(500),rs(1382),rs(461),rs(1337),rs(454),rs(1331),rs(330)))
+noord_polygon = polygon((20,50,120),(rs(1331),rs(330),rs(1237),rs(355),rs(1109),rs(432),rs(1136),rs(468),rs(1206),rs(462),rs(1208),rs(455),rs(1288),rs(443),rs(1337),rs(454)))
+delftshaven_polygon = polygon((20,50,120),(rs(1057),rs(479),rs(1034),rs(546),rs(1048),rs(582),rs(1038),rs(610),rs(1113),rs(596),rs(1227),rs(620),rs(1262),rs(614),rs(1244),rs(558),rs(1223),rs(553),rs(1247),rs(539),rs(1243),rs(456)))
+centrum_polygon = polygon((20,50,120),(rs(1262),rs(614),rs(1244),rs(558),rs(1223),rs(553),rs(1247),rs(539),rs(1243),rs(461),rs(1206),rs(462),rs(1208),rs(455),rs(1288),rs(443),rs(1337),rs(454),rs(1382),rs(461),rs(1397),rs(500),rs(1375),rs(506),rs(1322),rs(574)))
+feijenoord_polygon = polygon((20,50,120),(rs(1397),rs(500),rs(1375),rs(506),rs(1322),rs(574),rs(1262),rs(614),rs(1288),rs(642),rs(1378),rs(617),rs(1372),rs(694),rs(1361),rs(698),rs(1361),rs(720),rs(1417),rs(772),rs(1454),rs(735),rs(1487),rs(735),rs(1496),rs(698),rs(1450),rs(605),rs(1469),rs(593),rs(1453),rs(522)))
+ijsselmonde_polygon = polygon((20,50,120),(rs(1572),rs(594),rs(1489),rs(616),rs(1469),rs(593),rs(1450),rs(605),rs(1496),rs(698),rs(1487),rs(735),rs(1454),rs(735),rs(1417),rs(772),rs(1457),rs(824),rs(1524),rs(831),rs(1574),rs(813),rs(1657),rs(766),rs(1666),rs(779),rs(1682),rs(769),rs(1712),rs(606),rs(1625),rs(584)))
+charlois_polygon = polygon((20,50,120),(rs(1262),rs(614),rs(1288),rs(642),rs(1378),rs(617),rs(1372),rs(694),rs(1361),rs(698),rs(1361),rs(720),rs(1417),rs(772),rs(1457),rs(824),rs(1388),rs(820),rs(1315),rs(848),rs(1315),rs(869),rs(1242),rs(871),rs(1161),rs(839),rs(1201),rs(824),rs(1218),rs(800),rs(1201),rs(792),rs(1199),rs(763),rs(1246),rs(656),rs(1225),rs(646),rs(1227),rs(620)))
+waalhaven_polygon = polygon((20,50,120),(rs(1161),rs(839),rs(1201),rs(824),rs(1218),rs(800),rs(1201),rs(792),rs(1199),rs(763),rs(1246),rs(656),rs(1225),rs(646),rs(1227),rs(620),rs(1113),rs(596),rs(1038),rs(610),rs(954),rs(639),rs(862),rs(618),rs(885),rs(658),rs(917),rs(663),rs(921),rs(647),rs(972),rs(670),rs(962),rs(722),rs(937),rs(743),rs(934),rs(764),rs(923),rs(765),rs(927),rs(797),rs(1048),rs(795)))
 
 
-canvas.grid(row=1, column=0,sticky=N, ipadx=5000,ipady=500,rowspan=999)
+print(waalhaven_polygon)
+#mouse method
+def motion(event):
+    if event.x < 500 and event.y < 279: #the mouse positions
+        text.config(text=overschie) #changes the text that appears on the top
+    else:
+        text.config(text=rotterdam)
 
 
-canvas.bind('<Motion>',motion) #mouse method applied to the label(picture) object. Only when on the picture, the method will be activated
+#The text that will appear on top
+text = Label(root,width=0, height=1,text=rotterdam,font=("Helvetica",35,"bold")) #puts image on screen
+text.grid(row=0,column=0,sticky=N)
+
+
 #The main loop
 
 
@@ -247,5 +243,26 @@ Button7.grid(row=7, column=0, sticky=W,ipadx=screenx/34.5, ipady=screeny/150)
 Button8 = Button(root, text ="Back", command = backMethod,font=("arial",30,"bold"),bg="DeepSkyBlue2", fg="white")
 Button8.columnconfigure(0,weight=3)
 Button8.grid(row=8, column=0, sticky=W,ipadx=screenx/17.5, ipady=screeny/150)
+
+
+#With this the area's/polygons can be selected
+def click(event):
+    if str(canvas.find_withtag(CURRENT)) == "(6,)":
+        text.config(text=charlois)
+    else:
+        print(canvas.find_withtag(CURRENT))
+
+
+#the canvas where the polygons are drawed
+canvas.grid(row=2, column=0,sticky=N,rowspan=999,padx=55) #draws the canvas
+
+#when a polygon is clicked, the click method will be activated
+canvas.bind('<Button-1>', click) #mouse method applied to the label(picture) object. Only when on the picture, the method will be activated
+
+
+# prints the list of x and y coordinations of the polygon
+print(canvas.itemcget(feijenoord_polygon.shape, 'fill'))
+print(canvas.find_withtag(CURRENT))
+
 
 root.mainloop()
