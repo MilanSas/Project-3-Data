d62ec39378c97398dff47d94fcfec16000b5c17c
0927765 <0927765@hr.nl>
2017-04-06 13:32:05 +0200

    Code
    Tevredenheid cijfers toegevoegd en code  geschreven dat die geimport
    wordt in postgresql

--- ImportCSVtoPostgresql.py
+++ ImportCSVtoPostgresql.py
@@ -2,10 +2,12 @@ import pandas as pd
 gd = pd.read_csv('c:/Users/chris_e6ug8um/Documents/GitHub/Project-3-Data/Data/Geweldsdelicten.csv', sep=';')
 fd = pd.read_csv('c:/Users/chris_e6ug8um/Documents/GitHub/Project-3-Data/Data/Fietsendiefstal.csv', sep=';')
 do = pd.read_csv('c:/Users/chris_e6ug8um/Documents/GitHub/Project-3-Data/Data/Drugsoverlast.csv', sep=';')
+th = pd.read_csv('c:/Users/chris_e6ug8um/Documents/GitHub/Project-3-Data/Data/Tevredenheid.csv', sep=';')
 
 gd.columns = [c.lower() for c in gd.columns] #postgres doesn't like capitals or spaces
 fd.columns = [c.lower() for c in fd.columns]
 do.columns = [c.lower() for c in do.columns]
+th.columns = [c.lower() for c in th.columns]
 
 from sqlalchemy import create_engine
 engine = create_engine('postgresql://postgres:pgadmin2017@localhost:5432/StekOverflow')
@@ -13,4 +15,5 @@ engine = create_engine('postgresql://postgres:pgadmin2017@localhost:5432/StekOve
 gd.to_sql("geweldsdelicten", engine)
 fd.to_sql("fietsendiefstal", engine)
 do.to_sql("drugsoverlast", engine)
+th.to_sql("tevredenheid", engine)
 
