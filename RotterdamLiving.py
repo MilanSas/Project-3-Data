67ec41a3ec00e87a5e383b58f84cbd17acbd450f
Stefan Pesic <sp.pesic@live.nl>
2017-04-08 14:44:05 +0200

    Put polygons in function, edited text

--- RotterdamLiving.py
+++ RotterdamLiving.py
@@ -110,23 +110,54 @@ class polygon:
     def resetColur(self): #Method for when the user clicks another place, the previous selected place gets reseted
         pass
 
-
-
-overschie_polgon = polygon((20,50,120),(rs(904),rs(194),rs(947),rs(183),rs(955),rs(205),rs(1106),rs(109),rs(1176),rs(199),rs(1222),rs(163),rs(1242),rs(185),rs(1237),rs(355),rs(1109),rs(432),rs(1138),rs(471),rs(1057),rs(479),rs(1017),rs(403),rs(1059),rs(350),rs(1050),rs(345),rs(1003),rs(366),rs(964),rs(322),rs(949),rs(342)))
-hillegersberg_polygon = polygon((20,50,120),(rs(1242),rs(185),rs(1237),rs(355),rs(1520),rs(282),rs(1499),rs(189),rs(1475),rs(213),rs(1432),rs(195),rs(1428),rs(172),rs(1387),rs(141),rs(1382),rs(154),rs(1280),rs(90)))
-prins_alexander_polygon = polygon((20,50,120),(rs(1562),rs(454),rs(1661),rs(425),rs(1651),rs(350),rs(1693),rs(337),rs(1650),rs(227),rs(1663),rs(222),rs(1673),rs(233),rs(1821),rs(141),rs(1791),rs(116),rs(1815),rs(31),rs(1804),rs(12),rs(1617),rs(76),rs(1680),rs(145),rs(1627),rs(169),rs(1595),rs(134),rs(1499),rs(189)))
-kralingen_polygon = polygon((20,50,120),(rs(1521),rs(282),rs(1562),rs(454),rs(1538),rs(498),rs(1572),rs(594),rs(1489),rs(616),rs(1469),rs(593),rs(1453),rs(522),rs(1397),rs(500),rs(1382),rs(461),rs(1337),rs(454),rs(1331),rs(330)))
-noord_polygon = polygon((20,50,120),(rs(1331),rs(330),rs(1237),rs(355),rs(1109),rs(432),rs(1136),rs(468),rs(1206),rs(462),rs(1208),rs(455),rs(1288),rs(443),rs(1337),rs(454)))
-delftshaven_polygon = polygon((20,50,120),(rs(1057),rs(479),rs(1034),rs(546),rs(1048),rs(582),rs(1038),rs(610),rs(1113),rs(596),rs(1227),rs(620),rs(1262),rs(614),rs(1244),rs(558),rs(1223),rs(553),rs(1247),rs(539),rs(1243),rs(456)))
-centrum_polygon = polygon((20,50,120),(rs(1262),rs(614),rs(1244),rs(558),rs(1223),rs(553),rs(1247),rs(539),rs(1243),rs(461),rs(1206),rs(462),rs(1208),rs(455),rs(1288),rs(443),rs(1337),rs(454),rs(1382),rs(461),rs(1397),rs(500),rs(1375),rs(506),rs(1322),rs(574)))
-feijenoord_polygon = polygon((20,50,120),(rs(1397),rs(500),rs(1375),rs(506),rs(1322),rs(574),rs(1262),rs(614),rs(1288),rs(642),rs(1378),rs(617),rs(1372),rs(694),rs(1361),rs(698),rs(1361),rs(720),rs(1417),rs(772),rs(1454),rs(735),rs(1487),rs(735),rs(1496),rs(698),rs(1450),rs(605),rs(1469),rs(593),rs(1453),rs(522)))
-ijsselmonde_polygon = polygon((20,50,120),(rs(1572),rs(594),rs(1489),rs(616),rs(1469),rs(593),rs(1450),rs(605),rs(1496),rs(698),rs(1487),rs(735),rs(1454),rs(735),rs(1417),rs(772),rs(1457),rs(824),rs(1524),rs(831),rs(1574),rs(813),rs(1657),rs(766),rs(1666),rs(779),rs(1682),rs(769),rs(1712),rs(606),rs(1625),rs(584)))
-charlois_polygon = polygon((20,50,120),(rs(1262),rs(614),rs(1288),rs(642),rs(1378),rs(617),rs(1372),rs(694),rs(1361),rs(698),rs(1361),rs(720),rs(1417),rs(772),rs(1457),rs(824),rs(1388),rs(820),rs(1315),rs(848),rs(1315),rs(869),rs(1242),rs(871),rs(1161),rs(839),rs(1201),rs(824),rs(1218),rs(800),rs(1201),rs(792),rs(1199),rs(763),rs(1246),rs(656),rs(1225),rs(646),rs(1227),rs(620)))
-waalhaven_polygon = polygon((20,50,120),(rs(1161),rs(839),rs(1201),rs(824),rs(1218),rs(800),rs(1201),rs(792),rs(1199),rs(763),rs(1246),rs(656),rs(1225),rs(646),rs(1227),rs(620),rs(1113),rs(596),rs(1038),rs(610),rs(954),rs(639),rs(862),rs(618),rs(885),rs(658),rs(917),rs(663),rs(921),rs(647),rs(972),rs(670),rs(962),rs(722),rs(937),rs(743),rs(934),rs(764),rs(923),rs(765),rs(927),rs(797),rs(1048),rs(795)))
+#the polygons that make the rotterdam map possible
+def polygons():
+    overschie_polgon = polygon((20, 50, 120), (
+    rs(904), rs(194), rs(947), rs(183), rs(955), rs(205), rs(1106), rs(109), rs(1176), rs(199), rs(1222), rs(163),
+    rs(1242), rs(185), rs(1237), rs(355), rs(1109), rs(432), rs(1138), rs(471), rs(1057), rs(479), rs(1017), rs(403),
+    rs(1059), rs(350), rs(1050), rs(345), rs(1003), rs(366), rs(964), rs(322), rs(949), rs(342)))
+    hillegersberg_polygon = polygon((20, 50, 120), (
+    rs(1242), rs(185), rs(1237), rs(355), rs(1520), rs(282), rs(1499), rs(189), rs(1475), rs(213), rs(1432), rs(195),
+    rs(1428), rs(172), rs(1387), rs(141), rs(1382), rs(154), rs(1280), rs(90)))
+    prins_alexander_polygon = polygon((20, 50, 120), (
+    rs(1562), rs(454), rs(1661), rs(425), rs(1651), rs(350), rs(1693), rs(337), rs(1650), rs(227), rs(1663), rs(222),
+    rs(1673), rs(233), rs(1821), rs(141), rs(1791), rs(116), rs(1815), rs(31), rs(1804), rs(12), rs(1617), rs(76),
+    rs(1680), rs(145), rs(1627), rs(169), rs(1595), rs(134), rs(1499), rs(189)))
+    kralingen_polygon = polygon((20, 50, 120), (
+    rs(1521), rs(282), rs(1562), rs(454), rs(1538), rs(498), rs(1572), rs(594), rs(1489), rs(616), rs(1469), rs(593),
+    rs(1453), rs(522), rs(1397), rs(500), rs(1382), rs(461), rs(1337), rs(454), rs(1331), rs(330)))
+    noord_polygon = polygon((20, 50, 120), (
+    rs(1331), rs(330), rs(1237), rs(355), rs(1109), rs(432), rs(1136), rs(468), rs(1206), rs(462), rs(1208), rs(455),
+    rs(1288), rs(443), rs(1337), rs(454)))
+    delftshaven_polygon = polygon((20, 50, 120), (
+    rs(1057), rs(479), rs(1034), rs(546), rs(1048), rs(582), rs(1038), rs(610), rs(1113), rs(596), rs(1227), rs(620),
+    rs(1262), rs(614), rs(1244), rs(558), rs(1223), rs(553), rs(1247), rs(539), rs(1243), rs(456)))
+    centrum_polygon = polygon((20, 50, 120), (
+    rs(1262), rs(614), rs(1244), rs(558), rs(1223), rs(553), rs(1247), rs(539), rs(1243), rs(461), rs(1206), rs(462),
+    rs(1208), rs(455), rs(1288), rs(443), rs(1337), rs(454), rs(1382), rs(461), rs(1397), rs(500), rs(1375), rs(506),
+    rs(1322), rs(574)))
+    feijenoord_polygon = polygon((20, 50, 120), (
+    rs(1397), rs(500), rs(1375), rs(506), rs(1322), rs(574), rs(1262), rs(614), rs(1288), rs(642), rs(1378), rs(617),
+    rs(1372), rs(694), rs(1361), rs(698), rs(1361), rs(720), rs(1417), rs(772), rs(1454), rs(735), rs(1487), rs(735),
+    rs(1496), rs(698), rs(1450), rs(605), rs(1469), rs(593), rs(1453), rs(522)))
+    ijsselmonde_polygon = polygon((20, 50, 120), (
+    rs(1572), rs(594), rs(1489), rs(616), rs(1469), rs(593), rs(1450), rs(605), rs(1496), rs(698), rs(1487), rs(735),
+    rs(1454), rs(735), rs(1417), rs(772), rs(1457), rs(824), rs(1524), rs(831), rs(1574), rs(813), rs(1657), rs(766),
+    rs(1666), rs(779), rs(1682), rs(769), rs(1712), rs(606), rs(1625), rs(584)))
+    charlois_polygon = polygon((20, 50, 120), (
+    rs(1262), rs(614), rs(1288), rs(642), rs(1378), rs(617), rs(1372), rs(694), rs(1361), rs(698), rs(1361), rs(720),
+    rs(1417), rs(772), rs(1457), rs(824), rs(1388), rs(820), rs(1315), rs(848), rs(1315), rs(869), rs(1242), rs(871),
+    rs(1161), rs(839), rs(1201), rs(824), rs(1218), rs(800), rs(1201), rs(792), rs(1199), rs(763), rs(1246), rs(656),
+    rs(1225), rs(646), rs(1227), rs(620)))
+    waalhaven_polygon = polygon((20, 50, 120), (
+    rs(1161), rs(839), rs(1201), rs(824), rs(1218), rs(800), rs(1201), rs(792), rs(1199), rs(763), rs(1246), rs(656),
+    rs(1225), rs(646), rs(1227), rs(620), rs(1113), rs(596), rs(1038), rs(610), rs(954), rs(639), rs(862), rs(618),
+    rs(885), rs(658), rs(917), rs(663), rs(921), rs(647), rs(972), rs(670), rs(962), rs(722), rs(937), rs(743), rs(934),
+    rs(764), rs(923), rs(765), rs(927), rs(797), rs(1048), rs(795)))
 
 
 #The text that will appear on top
-text = Label(root,width=0, height=1,text=rotterdam,font=("Helvetica",35,"bold")) #puts image on screen
+text = Label(root,width=0, height=1,text="",font=("Helvetica",35,"bold")) #puts image on screen
 text.grid(row=0,column=0,sticky=N)
 
 #the text that appears on the bottom that displays the numbers in text, it tells a story
@@ -369,16 +400,17 @@ canvas.bind('<Button-1>', click, add="+") #mouse method applied to the label(pic
 
 #draws on top of the screen, this makes everything dissapear. The lower the code, the later it will get drawn
 #the canvas where the polygons are drawed
-canvas.grid(row=2, column=0,sticky=N,rowspan=999,padx=55) #draws the canvas
+theCanvas = canvas.grid(row=2, column=0,sticky=N,rowspan=999,padx=55) #draws the canvas
 
 
 
 #With this function, you know which button the from the drop down menu the user has pressed
 def menuSelector(event):
 
+
     global canvas
     if  str(variable.get()) == "Home":
-        print("I am home")
+        print("Home")
     elif  str(variable.get()) == "Woningadvies":
          woningsadvies()
     elif str(variable.get()) == "Percentages en cijfers":
@@ -389,6 +421,8 @@ def menuSelector(event):
         settings()
     elif str(variable.get()) == "Exit":
         root.destroy()
+
+
 
 
 
@@ -408,6 +442,7 @@ description_text.grid(row=1,column=0,sticky=W)
 
 #the page: Settings
 def settings():
+
     resolution_text = "Select your screen resolution"
     resolution1_text = Label(root, text=resolution_text, font=("Helvetica", 15, "bold"))  # puts image on screen
     resolution1_text.grid(row=2, column=0, sticky=W)
@@ -419,14 +454,18 @@ def settings():
     menu_button1.grid(row=3, column=0, sticky=N + W)
 
 
+
+
+
 #The page: About
 def about():
     if str(variable.get()) == "About":
-        about_text = "This application is made by students of the Hogeschool Rotterdam."
+        about_text = "This application is made by Informatica students of the Hogeschool Rotterdam."
         about1_text = "- Chris Santema"
         about2_text = "- Sebastiaan Van Etten"
         about3_text = "- Stefan Pesic"
         about4_text = "- Milan Sas"
+        about5_text = " © 2017 Team NoGo"
 
         about_information = Label(root, width=0, text=about_text, font=("Helvetica", 20, "bold"))  # puts text on screen
         about_information.grid(row=1, column=0, sticky=W)
@@ -442,22 +481,20 @@ def about():
 
         about_information4 = Label(root, width=0, text=about4_text, font=("Helvetica", 20, "bold"))
         about_information4.grid(row=5, column=0, sticky=W)
-    else:
-        print("About should be closed now.....")
-        return None
 
-
-
+        about_information5 = Label(root, width=0, text=about5_text, font=("Helvetica", 20, "bold"))
+        about_information5.grid(row=6, column=0, sticky=W)
 
 
 #The page: "Percentages en cijfers"
 def percentagesEnCijfers():
-    button9 = NewButton("Population", 1, 0, screenx / 19.5, screeny / 150)
-    button10 = NewButton("Environment", 2, 0, screenx / 19.5, screeny / 150)
-    button11 = NewButton("Safety", 3, 0, screenx / 19.5, screeny / 150)
-    button12 = NewButton("Traffic", 4, 0, screenx / 19.5, screeny / 150)
-    button13 = NewButton("Services", 5, 0, screenx / 19.5, screeny / 150)
-    button14 = NewButton("Overig", 6, 0, screenx / 19.5, screeny / 150)
+    button9 = NewButton("Population", 1, 0, screenx / 30, screeny / 150)
+    button10 = NewButton("Environment", 2, 0, screenx / 45, screeny / 150)
+    button11 = NewButton("Safety", 3, 0, screenx / 17, screeny / 150)
+    button12 = NewButton("Traffic", 4, 0, screenx / 17, screeny / 150)
+    button13 = NewButton("Services", 5, 0, screenx / 22, screeny / 150)
+    button14 = NewButton("Overig", 6, 0, screenx / 18, screeny / 150)
+
 
 def woningsadvies():
     # the woningadvies page
@@ -546,7 +583,6 @@ def woningsadvies():
         column=0, row=26, sticky=W)
     Radiobutton(root, indicatoron=False, text="7 or higher", variable=voorzieningen_radioButtons, value=4).grid(
         column=0, row=27, sticky=W)
-
 
 
 root.mainloop()
