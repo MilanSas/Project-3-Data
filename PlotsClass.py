46e1cf18162b82316db5e7dfe307b5fbadeb8288
Chris Santema <0927765@hr.nl>
2017-04-14 13:31:39 +0200

    Barchart now in class and both barchart+linechart are scaleable, added data for wijken/gebieden in rotterdam

--- PlotsClass.py
+++ PlotsClass.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sqlite3
+
 from sqlite3 import Error
 
 
-jaartaldata_tabelnamen = ["tevredenheid", "fietsendiefstal", "geweldsdelicten","drugsoverlast"]
-afkomstdata_kolomnamen = ['Nederlanders', 'Marokkanen', 'Turken', 'Kaapverdianen', 'Antilianen', 'Surinamers', 'Zuid-Europeanen', 'Overig']
-overigedata_tabelnamen = ["fiobj2016", "fisub2016", "si2016", "vi2016"]
+jaarTabelNamen = ["tevredenheid", "fietsendiefstal", "geweldsdelicten","drugsoverlast"]
+afkomstTabelNamen = ['Nederlanders', 'Marokkanen', 'Turken', 'Kaapverdianen', 'Antilianen', 'Surinamers', 'Zuid-Europeanen', 'Overig']
+
 
 class Plot:
-    def __init__(self, dbnaam, wknaam):
-        self.database_file = "Database\StekOverflow.db"
-        self.databasenaam = dbnaam
-        self.wijknaam = wknaam
-        self.plottitel = self.databasenaam + " in " + self.wijknaam
-        self.conn = self.create_connection(self.database_file)
+    def __init__(self, tabelnaam, wijknamenlist):
+        self.databaseFile = "Database\StekOverflow.db"
+        self.tabelNaam = tabelnaam
+        self.wijkNamenList = wijknamenlist
+        self.conn = self.create_connection(self.databaseFile)
         self.cur = self.conn.cursor()
 
     def create_connection(self,db):
@@ -26,29 +26,75 @@ class Plot:
         return None
 
 class PlotLineChart(Plot):
-    y = []
-    jaartallen = [2006, 2007, 2008, 2009, 2011]
+    def __init__(self, tabelnaam, wijknamenlist):
+        super().__init__(tabelnaam, wijknamenlist)
+        self.y = []
+        self.jaartallen = [2006, 2007, 2008, 2009, 2011]
+        self.g = globals()
 
-    def sql_query(self):
-        self.cur.execute("select data2006,data2007,data2008,data2009,data2011 from {} where wijknaam = '{}'".format(self.databasenaam, self.wijknaam))
-        self.wijkdata = self.cur.fetchone()
+        self.generate_empty_lists()
+        self.sql_query_linechart()
+        self.show_plot()
 
-        for i in range(len(self.wijkdata)):
-            self.y.append(self.wijkdata[i])
+    def generate_empty_lists(self):
+        for namen in (self.wijkNamenList):
+            self.g['data_{}'.format(namen)] = []
+
+    def sql_query_linechart(self):
+        for namen in (self.wijkNamenList):
+            self.cur.execute("select data2006,data2007,data2008,data2009,data2011 from"
+                             " {} where wijknaam = '{}'".format(self.tabelNaam, namen))
+            self.wijkData = self.cur.fetchone()
+            for i in range(len(self.wijkData)):
+                self.g['data_{}'.format(namen)].append(self.wijkData[i])
 
     def show_plot(self):
-        self.sql_query()
-        plt.plot(self.jaartallen, self.y, label=self.wijknaam, color='lightblue', linewidth=2)
-        for a, b in zip(self.jaartallen, self.y):
-            plt.text(a, b, str(b))
-
-        plt.ylabel(self.databasenaam + " %")
+        for namen in (self.wijkNamenList):
+            plt.plot(self.jaartallen, self.g['data_{}'.format(namen)], label=namen, linewidth=1)
+            for a, b in zip(self.jaartallen, self.g['data_{}'.format(namen)]):
+                plt.text(a, b, str(b))
+        plt.ylabel("Percentage")
         plt.xlabel('Jaren')
+        plt.title(self.tabelNaam)
         plt.legend()
-        plt.title(self.plottitel, bbox={'facecolor': '0.8', 'pad': 5})
         plt.show()
 
 
-Lineplot1 = PlotLineChart("tevredenheid","Carnisse")
-Lineplot1.show_plot()
+class PlotBarChart(Plot):
+    def __init__(self,tabelnaam, dataset, wijknamenlist):
+        self.dataSet = dataset
+        super().__init__(tabelnaam,wijknamenlist)
+        self.data = []
+        self.overigeTabelNamen = ["fiobj2016", "fisub2016", "si2016", "vi2016"]
+        self.aantalWijken = len(self.wijkNamenList)
+
+        self.sql_query_barchart()
+        self.show_plot()
+
+    def sql_query_barchart(self):
+        for namen in (self.wijkNamenList):
+            self.cur.execute("select {} from {} where wijknaam = '{}'".format(self.dataSet, self.tabelNaam, namen))
+            b = self.cur.fetchone()
+            self.data.append(b[0])
+
+    def show_plot(self):
+        y_pos = np.arange(len(self.wijkNamenList))
+
+        plt.bar(y_pos, self.data, align='center', alpha=0.5)
+        for a, b in zip(y_pos, self.data):
+            plt.text(a, b, str(b))
+
+        plt.xticks(y_pos, self.wijkNamenList)
+        plt.ylabel('Cijfer')
+        plt.title(self.dataSet)
+
+        plt.show()
+
+
+t = ["Carnisse", "Bospolder", "Spangen", "Tarwewijk", "Zuidwijk" ]
+PlotLineChart("geweldsdelicten", t)
+
+
+PlotBarChart("fiobj2016", "fysiekeindex", t)
+
 
